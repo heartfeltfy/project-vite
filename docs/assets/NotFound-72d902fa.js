5cad14cbfec6848f7faa2982c85e2c472f836b0c
chenmf <15239905718@163.com>
2023-11-27 14:28:01 +0800

    refactor：优化打包结果

---
+++ docs/assets/NotFound-72d902fa.js
@@ -0,0 +1,1 @@
+import{_ as o,z as t,l as e}from"./index-cde6c794.js";const n={},c={class:"tiger-notfound"};function s(r,_){return e(),t("div",c,"NotFound")}const d=o(n,[["render",s]]);export{d as default};
