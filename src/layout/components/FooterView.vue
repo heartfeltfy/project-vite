d2b6d4cd0eef220f4b133d4665a8416164a0eaf3
heartfeltfy <15239905718@163.com>
2024-05-30 11:01:07 +0800

    feat：配置tailwindcss

---
+++ src/layout/components/FooterView.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup="">
+
+</script>
+<template>
+  <div class="footer-wrapper">
+    <p>footer</p>
+    <github-logo />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/layout/components/HeaderView.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup="">
+
+</script>
+<template>
+  <div class="h-14">
+    <p class="text-2xl">header</p>
+    <github-logo />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
